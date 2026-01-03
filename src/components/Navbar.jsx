fac8dc2600bfe1c96664563365d430263e25f54e
Trapti <traptivishwakarma2007@gmail.com>
2026-01-03 21:53:36 +0530

    Finalize agency.ai React application with full UI and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 import assets from '../../assets/assets'
 import ThemeToggleBtn from './ThemeToggleBtn'
+import { easeOut, motion } from 'framer-motion'
 
 const Navbar = ({ theme, setTheme }) => {
 
     const [sidebarOpen, setSidebarOpen] = useState(false)
 
     return (
-        <div>
+        <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            translate={{ duration: 0.6, ease: 'easeOut' }}
+        >
+
             <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white dark:bg-gray-900/70">
                 <img src={theme === 'dark' ? assets.logo_dark : assets.logo}
                     className='w-32 sm:w-40'
@@ -17,14 +23,14 @@ const Navbar = ({ theme, setTheme }) => {
 
                     <img src={assets.close_icon} alt="" className='w-5 absolute right-4 top-4 sm:hidden' onClick={() => setSidebarOpen(false)} />
 
-                    <a onClick={()=>{setSidebarOpen(false)}} href="#" className='sm:hover:border-b'>Home</a>
-                    <a onClick={()=>{setSidebarOpen(false)}} href="#services" className='sm:hover:border-b'>Services</a>
-                    <a onClick={()=>{setSidebarOpen(false)}} href="#our-work" className='sm:hover:border-b'>Our Work</a>
-                    <a onClick={()=>{setSidebarOpen(false)}} href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
+                    <a onClick={() => { setSidebarOpen(false) }} href="#" className='sm:hover:border-b'>Home</a>
+                    <a onClick={() => { setSidebarOpen(false) }} href="#services" className='sm:hover:border-b'>Services</a>
+                    <a onClick={() => { setSidebarOpen(false) }} href="#our-work" className='sm:hover:border-b'>Our Work</a>
+                    <a onClick={() => { setSidebarOpen(false) }} href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
                 </div>
 
                 <div className='flex items-center gap-2 sm:gap-4'>
-                <ThemeToggleBtn theme={theme} setTheme={setTheme}/>
+                    <ThemeToggleBtn theme={theme} setTheme={setTheme} />
 
                     <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={() => setSidebarOpen(true)} className='w-8 sm:hidden' />
 
@@ -34,7 +40,7 @@ const Navbar = ({ theme, setTheme }) => {
                     </a>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,32 +1,38 @@
+import { motion } from 'framer-motion'
 import React, { useRef, useState } from 'react'
 
-const ServiceCard = ({service, index}) => {
+const ServiceCard = ({ service, index }) => {
 
-    const [position, setPosition] = useState({x:0, y:0})
-    const [visible, setVisible] = useState(false)
-    const divRef = useRef(null)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [visible, setVisible] = useState(false)
+  const divRef = useRef(null)
 
-    const handleMouseMove = (e)=>{
-      const bounds = divRef.current.getBoundingClientRect()
-      
-      setPosition({x: e.clientX - bounds.left, y: e.clientY - bounds.top})
-    }
+  const handleMouseMove = (e) => {
+    const bounds = divRef.current.getBoundingClientRect()
+
+    setPosition({ x: e.clientX - bounds.left, y: e.clientY - bounds.top })
+  }
 
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10'  onMouseEnter={()=>setVisible(true)}  onMouseLeave={()=>setVisible(false)}  ref={divRef} onMouseMove={handleMouseMove}>
-        <div className= {`pointer-events-none blur-2xl rounded-full bg-linear-to-r from-blue-500 to bg-purple-500 w-75 absolute z-0 transition-opacity duration-500 h-75 mix-blend-lighten opacity-70 ${visible ? 'opacity-70' : 'opacity-0'}`}  style={{top:position.y - 150, left:position.x - 150}} />
-            <div className="flex items-center gap-10 p-8 hover:p-7.5
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.2 }}
+      viewport={{ once: true }}
+      className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10' onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
+      <div className={`pointer-events-none blur-2xl rounded-full bg-linear-to-r from-blue-500 to bg-purple-500 w-75 absolute z-0 transition-opacity duration-500 h-75 mix-blend-lighten opacity-70 ${visible ? 'opacity-70' : 'opacity-0'}`} style={{ top: position.y - 150, left: position.x - 150 }} />
+      <div className="flex items-center gap-10 p-8 hover:p-7.5
              hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative">
-                <div className="bg-gray-100 dark:bg-gray-700 rounded-full ">
-                    <img src={service.icon} alt="" className='max-w-24 bg-white dark:bg-gray-900 rounded-full m-2'/>
-                </div>
-                <div className="flex-1">
-                    <h3 className='font-bold'>{service.title}</h3>
-                    <p className="text-sm mt-2">{service.description}</p>
-                </div>
-            </div>
-        
-    </div>
+        <div className="bg-gray-100 dark:bg-gray-700 rounded-full ">
+          <img src={service.icon} alt="" className='max-w-24 bg-white dark:bg-gray-900 rounded-full m-2' />
+        </div>
+        <div className="flex-1">
+          <h3 className='font-bold'>{service.title}</h3>
+          <p className="text-sm mt-2">{service.description}</p>
+        </div>
+      </div>
+
+    </motion.div>
   )
 }
 
